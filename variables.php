5dfa7c47d8ee838e3d6add54c1be2bf6391f7baf
root <root@localhost.localdomain>
2017-01-18 23:33:20 +0500

    Use of Print and echo

---
+++ variables.php
@@ -0,0 +1,21 @@
+ <html>
+<body>
+<?php
+ error_reporting(0);//to enable error notification
+$num1=123;
+$num2=321;
+
+
+$string="my first string";//there is no need to declar type 
+$char="a";
+echo"<strong>"."Numeric "."</strong>".$num1."<br>";
+echo"<strong>"."Numeric "."</strong>".$num2."<br>";
+
+echo"<strong>"."num1 + num2 ="."</strong>".$num1+$num2."<br>";//adding two numbers
+echo"<strong>"."Alphanumberic "."</strong>".$string."<br>";
+echo"<strong>"."Character "."</strong>".$char."<br>";
+
+?>
+
+</body>
+</html>
