1620cb187bedfe54a99f6e1b59f124f256d09f78
root <root@localhost.localdomain>
2017-01-18 23:26:54 +0500

    Use of Print and echo

---
+++ print_and_echo.php
@@ -0,0 +1,16 @@
+ <html>
+<body>
+<?php
+ error_reporting(0);//to enable error notification
+/*print and echo both are same commands use to ourput data*/
+//syntaxt of print
+print("I am a print  <br>");
+$num=90;
+print($num."<br>");
+//syntaxt of echo 
+echo("I am a echo<br> ");
+echo($num);
+?>
+
+</body>
+</html>
