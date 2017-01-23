4b474e4fb7455507a20d09599d6a5a0c0fff40f3
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-01-23 17:50:18 +0500

    Use of Basic operators

---
+++ OPerators.php
@@ -0,0 +1,21 @@
+ <html>
+<body>
+<?php
+ error_reporting(0);//to enable error notification
+
+ //Use of basic Operators
+$num1=4;
+$num2=6;
+$result = $num1 + $num2;
+$mul = $num1 * $num2;
+$squre = $num1 ** $num2;
+$div=$num1/$num2;
+$rem=$num1%$num2;
+echo "$num1 + $num2  = $result"."<br>";
+echo "$num1 * $num2  = $mul"."<br>";
+echo "$num1 ** $num2  = $squre"."<br>";
+echo "$num1 / $num2  = $div"."<br>";
+echo "$num1 % $num2  = $rem"."<br>";
+?>
+</body>
+</html>
