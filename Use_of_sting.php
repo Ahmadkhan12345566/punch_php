4db6f69533fcbde2f760773f274e80f32c55e115
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-01-23 17:37:19 +0500

    Use of string and built in functions

---
+++ Use_of_sting.php
@@ -0,0 +1,11 @@
+ <html>
+<body>
+<?php
+ error_reporting(0);//to enable error notification
+ //Function to help strings
+echo "length =".strlen("length equals to")."<br>";//count the total number of values in that string
+echo "word count =".str_word_count("length equals to"); 
+
+?>
+</body>
+</html>
