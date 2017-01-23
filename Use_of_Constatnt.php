3f51c0f7bd14ef33666ab9fc47bc80d09b16cd8d
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-01-23 17:40:22 +0500

    use of constant

---
+++ Use_of_Constatnt.php
@@ -0,0 +1,12 @@
+ <html>
+<body>
+<?php
+ error_reporting(0);//to enable error notification
+
+ //Constants are global they can use any where in a program
+
+define("asif","making a web");//macroos
+echo asif;
+?>
+</body>
+</html>
