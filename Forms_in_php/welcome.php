f019b0fd2911b87d4c0e94faf8470e4dafb55613
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-01-23 17:57:35 +0500

    Basic of Forms

---
+++ Forms_in_php/welcome.php
@@ -0,0 +1,11 @@
+ <html>
+<body>
+<?php
+echo "Welcome"."<br>"; 
+ echo $_GET["name"]; 
+echo "Your email address is: "."<br>"; 
+echo $_GET["email"]."<br>"; 
+echo "you  are in the right place";
+?>
+</body>
+</html>
